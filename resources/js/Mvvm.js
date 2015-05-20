7eef711cd0f3b806df95d395401cf56a7dbcd024
Kara <kara@karajensenportfolio.com>
2015-05-20 16:15:41 -0600

    Update Mvvm.js
    External JS FIles
    
    https://www.sencha.com/forum/showthread.php?152742-How-to-Load-Dinamically-(On-Demand)-external-javascript-with-ExtJS-4

--- resources/js/Mvvm.js
+++ resources/js/Mvvm.js
@@ -1,7 +1,7 @@
 //Tools Grab Object References Relative To Other Object References if in MVVM Mode
 
 Mvvm = {
-  _checkForParentViewModel - function(component) {
+  _checkForParentViewModel: function(component) {
     var parent = component ? component.getViewModel().getParent() : this.getViewModel().getParent();
     if (parent) return true; 
     else return false;
