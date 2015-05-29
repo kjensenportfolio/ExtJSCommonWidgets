731618962ec0e2743e21f8e63be94d5f8e361171
Kara <kara@karajensenportfolio.com>
2015-05-29 14:59:11 -0600

    Update Developer.js

--- resources/js/Developer.js
+++ resources/js/Developer.js
@@ -16,6 +16,35 @@ Developer = {
     extendSession: function() {
         am = Ext.ux.ActivityMonitor;
         am.ui.initEvent('mousemove', am.captureActivity, am);
-    }
+    },
+    
+    _applicationName: function() {
+        var apps = ['one', 'two', 'three', 'four'];
+        apps.forEach(function(app) {
+            if (Ext.isObject(this[app])) {
+                [app][0];
+                console.log([app][0]);
+                return [app][0];
+            }
+        });
+    },
+
+    getViewPort: function() {
+        return Developer._applicationName().getApplication().getMainView();
+    },
+
+    lookupReference: function(referencekey) {
+        return Developer._applicationName().getApplication().getMainView().lookupReference(referencekey);
+    },
+
+    //lookupWindowReference: function('') {
+    //return Developer._applicationName().getApplication().lookupReference('');
+    //},
+
+    //deleteMultipleItems: function() {
+    //var ref =
+    //return lookupReference();
+    //}
+
     
 };
